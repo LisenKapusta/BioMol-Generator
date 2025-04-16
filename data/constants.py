ab07ace6486cd3673ba7e68ef2ed35e231b61a96
LisenKapusta <elizavetaholodkova@gmail.com>
2025-04-16 22:23:56 +0300

    поправка Dataset

--- data/constants.py
+++ data/constants.py
@@ -4,9 +4,7 @@ LOCAL_PATH = Path(__file__).parents[1]
 DATASET_PATH = LOCAL_PATH / 'data/dataset.parquet'
 
 LOCAL_MODELS_PATH = LOCAL_PATH / 'models'
-LOCAL_MODELS_PATH.mkdir(exist_ok=True, parants=True)
+LOCAL_MODELS_PATH.mkdir(exist_ok=True, parents=True)
 
 CHECKPOINTS_PATH = LOCAL_PATH / 'model_training/model_epoch_save'
-CHECKPOINTS_PATH.mkdir(exist_ok=True, parants=True)
-
-print(LOCAL_PATH)
+CHECKPOINTS_PATH.mkdir(exist_ok=True, parents=True)
--- data/load_data.py
+++ data/load_data.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from datasets import load_dataset
-from .constants import DATASET_PATH
+from constants import DATASET_PATH
 
 ds = load_dataset("BALM/BALM-benchmark", "BindingDB_filtered")
 parquet_filename = DATASET_PATH
 # Преобразование каждого раздела в Parquet
 for split, dataset in ds.items():
-    dataset.to_parquet(parquet_filename, index=False)
+    dataset.to_parquet(parquet_filename)
     print(f"Saved {parquet_filename}")
+    
