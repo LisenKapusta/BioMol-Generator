eab252a6eee97cb8824d791b46d9fd96de73e4dc
LisenKapusta <elizavetaholodkova@gmail.com>
2025-04-16 20:06:47 +0300

    init commit

---
+++ data/load_data.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from datasets import load_dataset
+from .constants import DATASET_PATH
+
+ds = load_dataset("BALM/BALM-benchmark", "BindingDB_filtered")
+parquet_filename = DATASET_PATH
+# Преобразование каждого раздела в Parquet
+for split, dataset in ds.items():
+    dataset.to_parquet(parquet_filename, index=False)
+    print(f"Saved {parquet_filename}")
