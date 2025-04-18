1e2bc6b77e2b58bd02b3ec45bd637a2e4ec2ac78
LisenKapusta <elizavetaholodkova@gmail.com>
2025-04-19 00:52:41 +0300

    работа с промптингом

--- data/constants.py
+++ data/constants.py
@@ -8,3 +8,11 @@ LOCAL_MODELS_PATH.mkdir(exist_ok=True, parents=True)
 
 CHECKPOINTS_PATH = LOCAL_PATH / 'model_training/model_epoch_save'
 CHECKPOINTS_PATH.mkdir(exist_ok=True, parents=True)
+
+DOCUMENT_FILE_PATH = LOCAL_PATH / 'data/document.txt'
+SYSTEM = LOCAL_PATH / 'data/system.txt'
+PREPROMPT = LOCAL_PATH / 'data/preprompt.txt'
+POSTPROMPT = LOCAL_PATH / 'data/postprompt.txt'
+TEMP = 0.7
+
+LOCAL_SERVER_URL = 'http://10.147.19.239:1234'
